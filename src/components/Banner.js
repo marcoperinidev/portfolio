d6447cf1449b626e888537c3a2e593bc977137c6
marcoperinidev <info@mpdev.me>
2023-01-22 20:05:03 +0100

    migliorato effetto typewriter

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,50 +4,11 @@ import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { Typewriter, Cursor } from 'react-simple-typewriter';
 
-      
+
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer" ];
-  const period = 2000;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(1500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
+  
   return (
     <section className="banner" id="home">
       <Container>
@@ -57,7 +18,19 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Marco,`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer" ]'><span className="wrap">{text}</span></span></h1>
+                        <h1>Hey! I'm Marco{<br/>}
+                          <span>
+                            <Typewriter
+                              words={['Web Developer','Web Designer']}
+                              loop={0}
+                              typeSpeed={100}
+                              deleteSpeed={100}
+                            />
+                          </span>
+                    <span>
+                        <Cursor cursorStyle='|'/>
+                    </span>
+                </h1>
                   <p className="p-about"><strong>Solving problems</strong> since 1989. Given that I thought development would've been a good life choice. So after a coding bootcamp, dedication and no sleep I try to help others building their presence on the web taking care of all the process. </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
@@ -75,4 +48,4 @@ export const Banner = () => {
       </Container>
     </section>
   )
-}
+};
