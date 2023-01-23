7b193231de803d82c8c9a12ba8e0bbf79877257a
marcoperinidev <info@mpdev.me>
2023-01-23 10:17:18 +0100

    added target rel on links and removed 2 components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/pmfullstack/" target={"_blank"}><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://github.com/marcoperinidev"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/marco_perini_dev/"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/pmfullstack/" target={"_blank"} rel="noreferrer"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/marcoperinidev" target={"_blank"} rel="noreferrer"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/marco_perini_dev/" target={"_blank"} rel="noreferrer"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Marco Perini Copyright 2022. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,9 +48,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/pmfullstack/"><img src={navIcon1} alt="LinkedIn" /></a>
-                <a href="https://github.com/marcoperinidev"><img src={navIcon2} alt="GitHub" /></a>
-                <a href="https://www.instagram.com/marco_perini_dev/"><img src={navIcon3} alt="Instagram" /></a>
+                <a href="https://www.linkedin.com/in/pmfullstack/" target={"_blank"} rel="noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://github.com/marcoperinidev" target={"_blank"} rel="noreferrer"><img src={navIcon2} alt="GitHub" /></a>
+                <a href="https://www.instagram.com/marco_perini_dev/" target={"_blank"} rel="noreferrer"><img src={navIcon3} alt="Instagram" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
