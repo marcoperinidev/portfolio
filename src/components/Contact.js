52ddd23761529a94095be1e4ba88587e440c08bb
marcoperinidev <info@mpdev.me>
2023-03-07 04:33:32 +0100

    button and download cv works fine

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,7 +41,7 @@ export const Contact = () => {
                     <span className="contact-ico"> <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-envelope-heart" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l3.235 1.94a2.76 2.76 0 0 0-.233 1.027L1 5.384v5.721l3.453-2.124c.146.277.329.556.55.835l-3.97 2.443A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741l-3.968-2.442c.22-.28.403-.56.55-.836L15 11.105V5.383l-3.002 1.801a2.76 2.76 0 0 0-.233-1.026L15 4.217V4a1 1 0 0 0-1-1H2Zm6 2.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
                     </svg></span>
-                      <a href="mailto:info@mpdev.me">info@mpdev.me</a> 
+                      <a className="email-link" href="mailto:info@mpdev.me">info@mpdev.me</a> 
                       <br></br>
                       <span className="contact-ico2"> <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-phone-vibrate" viewBox="0 0 16 16">
                       <path d="M10 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4zM6 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H6z"/>
@@ -52,7 +52,7 @@ export const Contact = () => {
                   </Row>
                   <Row>
                     <Col size={12} sm={6} className="contact-info">
-                    <button onClick={handleDownload}>Download CV</button>
+                    <button className="btnD btn-1 btn-1e" onClick={handleDownload}>Download CV</button>
                     </Col>
                   </Row>
               </div>}
